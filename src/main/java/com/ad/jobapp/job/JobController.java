898468bba184b98da5e10f20014f4d11e71735f0
AdLKL <lkl88adnane@gmail.com>
2024-09-11 19:28:02 +0100

    Added Job Service

--- src/main/java/com/ad/jobapp/job/JobController.java
+++ src/main/java/com/ad/jobapp/job/JobController.java
@@ -1,17 +1,55 @@
 package com.ad.jobapp.job;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/jobs")
 public class JobController {
-    private List<Job> jobs = new ArrayList<Job>();
+    private final JobService jobService;
 
-    @GetMapping("/jobs")
-    public List<Job> findAll() {
-        return jobs;
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Job>> findAll() {
+        return ResponseEntity.ok(jobService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createJob(@RequestBody Job job) {
+        jobService.createJob(job);
+        return new ResponseEntity<>("Job created", HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Job> findById(@PathVariable Long id) {
+        Job job = jobService.getJobById(id);
+        if (job == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(job, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteJob(@PathVariable Long id) {
+        boolean deleted = jobService.deleteJobById(id);
+        if (deleted) {
+            return new ResponseEntity<>("Job deleted", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Job not found", HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateJob(@PathVariable Long id, @RequestBody Job job) {
+        boolean updated = jobService.updateJobById(id, job);
+        if (updated) {
+            return new ResponseEntity<>("Job updated", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Job not found", HttpStatus.NOT_FOUND);
     }
 }
